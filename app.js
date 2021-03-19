2cc17aea308f4f5b8f419b463cfb525a1010568b
Logan8192 <vcubilla@protonmail.com>
2021-03-18 23:48:21 -0500

    Feat: Add Spanish translation with i18next

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const session = require('express-session');
 const path = require('path');
 const logger = require('morgan');
 const passport = require('passport');
+const i18next = require('i18next');
+const i18nextMiddleware = require('i18next-http-middleware');
+const i18nextBackend = require('i18next-fs-backend');
 const isAuthenticated = require('./middlewares/isAuthenticated');
 require('dotenv').config();
 
@@ -14,7 +17,13 @@ const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-app.use(session({ secret: process.env.CLIENT_SECRET, resave: true, saveUninitialized: true }));
+app.use(
+  session({
+    secret: process.env.CLIENT_SECRET,
+    resave: true,
+    saveUninitialized: true,
+  }),
+);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -22,14 +31,44 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
 app.use(passport.session());
 
+i18next
+  .use(i18nextBackend)
+  .use(i18nextMiddleware.LanguageDetector)
+  .init({
+    ns: ['albums', 'artists', 'common', 'index', 'navbar', 'tracks'],
+    lng: 'us',
+    fallbackLng: 'es',
+    preload: ['en', 'es'],
+    backend: {
+      loadPath: path.join(__dirname, 'public/locales/{{lng}}/{{ns}}.json'),
+    },
+    detection: {
+      order: ['querystring', 'cookie'],
+      caches: ['cookie'],
+      lookupQuerystring: 'locale',
+      lookupCookie: 'locale',
+      ignoreCase: true,
+      cookieSecure: true,
+    },
+  });
+app.use(i18nextMiddleware.handle(i18next));
+
 // Setting routes
 app.use('/', require('./routes/index'));
 app.use('/artists', isAuthenticated, require('./routes/artists.route'));
 app.use('/albums', isAuthenticated, require('./routes/albums.route'));
 app.use('/tracks', isAuthenticated, require('./routes/tracks.route'));
 
-app.use('/bulma', express.static(path.join(__dirname, '/node_modules/bulma/css/')));
-app.use('/font-awesome', express.static(path.join(__dirname, '/node_modules/@fortawesome/fontawesome-free/')));
+app.use(
+  '/bulma',
+  express.static(path.join(__dirname, '/node_modules/bulma/css/')),
+);
+app.use(
+  '/font-awesome',
+  express.static(
+    path.join(__dirname, '/node_modules/@fortawesome/fontawesome-free/'),
+  ),
+);
 
 passport.serializeUser((user, done) => {
   done(null, user);
@@ -68,7 +107,8 @@ passport.use(
 
 app.get(
   '/callback',
-  passport.authenticate('spotify', { failureRedirect: '/' }), (req, res) => {
+  passport.authenticate('spotify', { failureRedirect: '/' }),
+  (req, res) => {
     res.redirect('/');
   },
 );
@@ -77,6 +117,11 @@ app.get(
 app.get('/logout', (req, res) => {
   req.logout();
   res.redirect('/');
+});
+
+app.get('/lang/:code', (req, res) => {
+  req.i18n.changeLanguage(req.params.code);
+  res.redirect('back');
 });
 
 // catch 404 and forward to error handler
--- controllers/albums.controller.js
+++ controllers/albums.controller.js
@@ -3,44 +3,59 @@ const countriesJSON = require('../public/json/countries_ISO_3166-1_alpha-2.json'
 
 albumsController.getAlbums = async (req, res) => {
   if (req.query.search !== undefined) {
-    await req.app.locals.spotifyApi.searchAlbums(req.query.search, { limit: 25, market: ['JP', 'PA', 'US'] })
-      .then((data) => {
-        res.render('albums/albumsSearch',
-          {
+    await req.app.locals.spotifyApi
+      .searchAlbums(req.query.search, { limit: 25, market: ['JP', 'PA', 'US'] })
+      .then(
+        (data) => {
+          res.render('albums/albumsSearch', {
             searchResults: data.body.albums.items,
             searchValue: req.query.search,
-            title: 'Spotify API Showcase - Search Albums Results',
+            title: `${req.t('common:app_name')} -
+                    ${req.t('albums:search_albums')}`,
             user: req.user,
           });
-      }, (err) => {
-        res.render('error', { error: err });
-      });
+        },
+        (err) => {
+          res.render('error', { error: err });
+        },
+      );
   } else {
-    res.render('albums/albumsSearch', { user: req.user, title: 'Spotify API Showcase - Search Albums' });
+    res.render('albums/albumsSearch', {
+      user: req.user,
+      title: `${req.t('common:app_name')} -
+      ${req.t('albums:search_albums')}`,
+    });
   }
 };
 
 albumsController.getAlbum = async (req, res) => {
   const album = {};
-  await req.app.locals.spotifyApi.getAlbum(req.params.id)
-    .then((data) => {
+  await req.app.locals.spotifyApi.getAlbum(req.params.id).then(
+    (data) => {
       album.info = data.body;
-    }, (err) => {
+    },
+    (err) => {
       res.render('error', { error: err });
-    });
-  await req.app.locals.spotifyApi.getAlbumTracks(req.params.id, { limit: 50 })
-    .then((data) => {
-      album.tracks = data.body;
-      res.render('albums/albumDetails',
-        {
+    },
+  );
+  await req.app.locals.spotifyApi
+    .getAlbumTracks(req.params.id, { limit: 50 })
+    .then(
+      (data) => {
+        album.tracks = data.body;
+        res.render('albums/albumDetails', {
           album,
           countriesJSON,
-          title: `Spotify API Showcase - Album - ${album.info.name}`,
+          title: `${req.t('common:app_name')} -
+          ${req.t('common:album')} -
+          ${album.info.name}`,
           user: req.user,
         });
-    }, (err) => {
-      res.render('error', { error: err });
-    });
+      },
+      (err) => {
+        res.render('error', { error: err });
+      },
+    );
 };
 
 module.exports = albumsController;
